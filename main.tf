952825a396f30d8d6104613f0ba534d4f47aa421
Daniel J <daniel@finisterra.io>
2024-02-06 10:10:04 -0500

    feat: initial release

---
+++ main.tf
@@ -0,0 +1,238 @@
+resource "aws_launch_template" "this" {
+  count = var.create ? 1 : 0
+
+  instance_type = var.instance_type
+
+  dynamic "block_device_mappings" {
+    for_each = var.block_device_mappings
+
+    content {
+      device_name = try(block_device_mappings.value.device_name, null)
+
+      dynamic "ebs" {
+        for_each = lookup(block_device_mappings.value, "ebs", null) != null ? block_device_mappings.value.ebs : []
+
+        content {
+          delete_on_termination = try(ebs.value.delete_on_termination, null)
+          encrypted             = try(ebs.value.encrypted, null)
+          iops                  = try(ebs.value.iops, null)
+          kms_key_id            = try(ebs.value.kms_key_id, null)
+          snapshot_id           = try(ebs.value.snapshot_id, null)
+          throughput            = try(ebs.value.throughput, null)
+          volume_size           = try(ebs.value.volume_size, null)
+          volume_type           = try(ebs.value.volume_type, null)
+        }
+      }
+
+      no_device    = try(block_device_mappings.value.no_device, null)
+      virtual_name = try(block_device_mappings.value.virtual_name, null)
+    }
+  }
+
+  dynamic "capacity_reservation_specification" {
+    for_each = length(var.capacity_reservation_specification) > 0 ? [var.capacity_reservation_specification] : []
+
+    content {
+      capacity_reservation_preference = try(capacity_reservation_specification.value.capacity_reservation_preference, null)
+
+      dynamic "capacity_reservation_target" {
+        for_each = try([capacity_reservation_specification.value.capacity_reservation_target], [])
+
+        content {
+          capacity_reservation_id                 = try(capacity_reservation_target.value.capacity_reservation_id, null)
+          capacity_reservation_resource_group_arn = try(capacity_reservation_target.value.capacity_reservation_resource_group_arn, null)
+        }
+      }
+    }
+  }
+
+  dynamic "cpu_options" {
+    for_each = length(var.cpu_options) > 0 ? [var.cpu_options] : []
+
+    content {
+      core_count       = try(cpu_options.value.core_count, null)
+      threads_per_core = try(cpu_options.value.threads_per_core, null)
+    }
+  }
+
+  dynamic "credit_specification" {
+    for_each = length(var.credit_specification) > 0 ? [var.credit_specification] : []
+
+    content {
+      cpu_credits = try(credit_specification.value.cpu_credits, null)
+    }
+  }
+
+  default_version         = var.default_version
+  description             = var.description
+  disable_api_termination = var.disable_api_termination
+  ebs_optimized           = var.ebs_optimized
+
+  dynamic "elastic_gpu_specifications" {
+    for_each = var.elastic_gpu_specifications
+
+    content {
+      type = elastic_gpu_specifications.value.type
+    }
+  }
+
+  dynamic "elastic_inference_accelerator" {
+    for_each = length(var.elastic_inference_accelerator) > 0 ? [var.elastic_inference_accelerator] : []
+
+    content {
+      type = elastic_inference_accelerator.value.type
+    }
+  }
+
+  dynamic "enclave_options" {
+    for_each = length(var.enclave_options) > 0 ? [var.enclave_options] : []
+
+    content {
+      enabled = enclave_options.value.enabled
+    }
+  }
+
+  dynamic "iam_instance_profile" {
+    for_each = var.iam_instance_profile
+    content {
+      name = try(iam_instance_profile.value.name, null)
+      arn  = try(iam_instance_profile.value.arn, null)
+    }
+  }
+
+  image_id = var.ami_id
+
+  dynamic "instance_market_options" {
+    for_each = length(var.instance_market_options) > 0 ? [var.instance_market_options] : []
+
+    content {
+      market_type = try(instance_market_options.value.market_type, null)
+
+      dynamic "spot_options" {
+        for_each = try([instance_market_options.value.spot_options], [])
+
+        content {
+          block_duration_minutes         = try(spot_options.value.block_duration_minutes, null)
+          instance_interruption_behavior = try(spot_options.value.instance_interruption_behavior, null)
+          max_price                      = try(spot_options.value.max_price, null)
+          spot_instance_type             = try(spot_options.value.spot_instance_type, null)
+          valid_until                    = try(spot_options.value.valid_until, null)
+        }
+      }
+    }
+  }
+
+  # # Set on node group instead
+  # instance_type = var.instance_type
+  kernel_id = var.kernel_id
+  key_name  = var.key_name
+
+  dynamic "license_specification" {
+    for_each = length(var.license_specifications) > 0 ? var.license_specifications : {}
+
+    content {
+      license_configuration_arn = license_specifications.value.license_configuration_arn
+    }
+  }
+
+  dynamic "maintenance_options" {
+    for_each = length(var.maintenance_options) > 0 ? [var.maintenance_options] : []
+
+    content {
+      auto_recovery = try(maintenance_options.value.auto_recovery, null)
+    }
+  }
+
+  dynamic "metadata_options" {
+    for_each = var.metadata_options
+
+    content {
+      http_endpoint               = try(metadata_options.value.http_endpoint, null)
+      http_protocol_ipv6          = try(metadata_options.value.http_protocol_ipv6, null)
+      http_put_response_hop_limit = try(metadata_options.value.http_put_response_hop_limit, null)
+      http_tokens                 = try(metadata_options.value.http_tokens, null)
+      instance_metadata_tags      = try(metadata_options.value.instance_metadata_tags, null)
+    }
+  }
+
+  dynamic "monitoring" {
+    for_each = var.enable_monitoring ? [true] : [false]
+
+    content {
+      enabled = monitoring.value
+    }
+  }
+
+  name = var.name
+
+  dynamic "network_interfaces" {
+    for_each = var.network_interfaces
+    content {
+      associate_carrier_ip_address = try(network_interfaces.value.associate_carrier_ip_address, null)
+      associate_public_ip_address  = try(network_interfaces.value.associate_public_ip_address, null)
+      delete_on_termination        = try(network_interfaces.value.delete_on_termination, null)
+      description                  = try(network_interfaces.value.description, null)
+      device_index                 = try(network_interfaces.value.device_index, null)
+      interface_type               = try(network_interfaces.value.interface_type, null)
+      ipv4_address_count           = try(network_interfaces.value.ipv4_address_count, null)
+      ipv4_addresses               = try(network_interfaces.value.ipv4_addresses, [])
+      ipv4_prefix_count            = try(network_interfaces.value.ipv4_prefix_count, null)
+      ipv4_prefixes                = try(network_interfaces.value.ipv4_prefixes, null)
+      ipv6_address_count           = try(network_interfaces.value.ipv6_address_count, null)
+      ipv6_addresses               = try(network_interfaces.value.ipv6_addresses, [])
+      ipv6_prefix_count            = try(network_interfaces.value.ipv6_prefix_count, null)
+      ipv6_prefixes                = try(network_interfaces.value.ipv6_prefixes, [])
+      network_card_index           = try(network_interfaces.value.network_card_index, null)
+      network_interface_id         = try(network_interfaces.value.network_interface_id, null)
+      private_ip_address           = try(network_interfaces.value.private_ip_address, null)
+      security_groups              = try(network_interfaces.value.security_groups, null)
+    }
+  }
+
+  dynamic "placement" {
+    for_each = length(var.placement) > 0 ? [var.placement] : []
+
+    content {
+      affinity                = try(placement.value.affinity, null)
+      availability_zone       = try(placement.value.availability_zone, null)
+      group_name              = try(placement.value.group_name, null)
+      host_id                 = try(placement.value.host_id, null)
+      host_resource_group_arn = try(placement.value.host_resource_group_arn, null)
+      partition_number        = try(placement.value.partition_number, null)
+      spread_domain           = try(placement.value.spread_domain, null)
+      tenancy                 = try(placement.value.tenancy, null)
+    }
+  }
+
+  dynamic "private_dns_name_options" {
+    for_each = length(var.private_dns_name_options) > 0 ? [var.private_dns_name_options] : []
+
+    content {
+      enable_resource_name_dns_aaaa_record = try(private_dns_name_options.value.enable_resource_name_dns_aaaa_record, null)
+      enable_resource_name_dns_a_record    = try(private_dns_name_options.value.enable_resource_name_dns_a_record, null)
+      hostname_type                        = try(private_dns_name_options.value.hostname_type, null)
+    }
+  }
+
+  ram_disk_id = var.ram_disk_id
+
+  dynamic "tag_specifications" {
+    for_each = var.tag_specifications
+
+    content {
+      resource_type = tag_specifications.value.resource_type
+      tags          = tag_specifications.value.tags
+    }
+  }
+
+  user_data              = var.user_data
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  tags = var.tags
+
+  instance_initiated_shutdown_behavior = var.instance_initiated_shutdown_behavior
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
